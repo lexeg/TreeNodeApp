e5919fd3456bb99b14704a8adee22dcaf2ba1793
apetrenko <apetrenko@gseis.ru>
2024-12-10 17:47:04 +0500

    Вынес в отдельный класс создание дерева

--- TreeNodeConsoleApp/Program.cs
+++ TreeNodeConsoleApp/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var tree = CreateTree();
+        var tree = TreeNodeBuilder.CreateBigTree();
         tree.Print();
         Console.WriteLine();
 
@@ -25,30 +25,6 @@ class Program
         Console.WriteLine();
         Console.WriteLine("Result:");
         newTree.First().Print();
-    }
-
-    private static TreeNode CreateTree()
-    {
-        var root = new TreeNode { Id = Guid.NewGuid(), Name = "Подразделение 1", Childs = new List<TreeNode>() };
-        var childOne = CreateChild("Подразделение 1.1", root.Id);
-        var childTwo = CreateChild("Подразделение 1.2", root.Id);
-        root.Childs.Add(childOne);
-        root.Childs.Add(childTwo);
-        root.Childs.Add(CreateChild("Подразделение 1.3", root.Id));
-
-        childOne.Childs.Add(CreateChild("Подразделение 1.1.1", childOne.Id));
-        childOne.Childs.Add(CreateChild("Подразделение 1.1.2", childOne.Id));
-        childOne.Childs.Add(CreateChild("Подразделение 1.1.3", childOne.Id));
-        childOne.Childs.Add(CreateChild("Подразделение 1.1.4", childOne.Id));
-
-        childTwo.Childs.Add(CreateChild("Подразделение 1.2.1", childTwo.Id));
-        childTwo.Childs.Add(CreateChild("Подразделение 1.2.2", childTwo.Id));
-        return root;
-    }
-
-    private static TreeNode CreateChild(string name, Guid rootId)
-    {
-        return new TreeNode { Id = Guid.NewGuid(), Name = name, ParentId = rootId, Childs = new List<TreeNode>() };
     }
 
     private static TreeNode[] BuildTree(TreeNode[] children, TreeNode[] sourceTree)
---
+++ TreeNodeConsoleApp/TreeNodeBuilder.cs
@@ -0,0 +1,48 @@
+﻿namespace TreeNodeConsoleApp;
+
+public static class TreeNodeBuilder
+{
+    public static TreeNode CreateTree()
+    {
+        var root = new TreeNode { Id = Guid.NewGuid(), Name = "Подразделение 1", Childs = new List<TreeNode>() };
+        var childOne = CreateChild("Подразделение 1.1", root.Id);
+        var childTwo = CreateChild("Подразделение 1.2", root.Id);
+        root.Childs.Add(childOne);
+        root.Childs.Add(childTwo);
+        root.Childs.Add(CreateChild("Подразделение 1.3", root.Id));
+
+        childOne.Childs.Add(CreateChild("Подразделение 1.1.1", childOne.Id));
+        childOne.Childs.Add(CreateChild("Подразделение 1.1.2", childOne.Id));
+        childOne.Childs.Add(CreateChild("Подразделение 1.1.3", childOne.Id));
+        childOne.Childs.Add(CreateChild("Подразделение 1.1.4", childOne.Id));
+
+        childTwo.Childs.Add(CreateChild("Подразделение 1.2.1", childTwo.Id));
+        childTwo.Childs.Add(CreateChild("Подразделение 1.2.2", childTwo.Id));
+        return root;
+    }
+
+    public static TreeNode CreateBigTree()
+    {
+        const int childrenCount = 1000;
+        var root = new TreeNode { Id = Guid.NewGuid(), Name = "Подразделение 1", Childs = new List<TreeNode>() };
+        var childOne = CreateChild("Подразделение 1.1", root.Id);
+        var childTwo = CreateChild("Подразделение 1.2", root.Id);
+        root.Childs.Add(childOne);
+        root.Childs.Add(childTwo);
+        root.Childs.Add(CreateChild("Подразделение 1.3", root.Id));
+
+        for (var i = 0; i < childrenCount; i++)
+        {
+            childOne.Childs.Add(CreateChild($"Подразделение 1.1.{i + 1}", childOne.Id));
+        }
+
+        childTwo.Childs.Add(CreateChild("Подразделение 1.2.1", childTwo.Id));
+        childTwo.Childs.Add(CreateChild("Подразделение 1.2.2", childTwo.Id));
+        return root;
+    }
+
+    private static TreeNode CreateChild(string name, Guid rootId)
+    {
+        return new TreeNode { Id = Guid.NewGuid(), Name = name, ParentId = rootId, Childs = new List<TreeNode>() };
+    }
+}
